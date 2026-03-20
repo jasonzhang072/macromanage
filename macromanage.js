7b67c65f59bfd83197e81e4158e2dfa6592407f9
Jason Zhang <jasonzhang072@gmail.com>
2026-03-20 15:28:59 -0700

    Update version to v3.7.0 - Time Slots Fixed + Smooth UI

--- macromanage.js
+++ macromanage.js
@@ -1,6 +1,6 @@
 class MacroManage {
     constructor() {
-        console.log('MacroManage v3.6.1 - Time Slot Fix');
+        console.log('MacroManage v3.7.0 - Time Slots Fixed + Smooth UI');
         this.currentTab = 'dashboard';
         this.user = null;
         this.events = [];
